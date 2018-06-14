4bf6c09c3581c7762ddcb88e9d34783be0f83b02
Amber Ostendorf <ambersed_2000@yahoo.com>
2018-06-14 15:32:10 -0600

    initial commit

---
+++ client/src/components/Blogs.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../App.css';
+
+componentDidMount() {
+axios.get()
+.then( res => {
+
+})
+.catch( err => {})
+
+}
+
+class Blogs extends React.Component {
+  render() {
+    return(
+      <div>
+        <h1>Blogs</h1>
+      </div>
+    )
+  }
+}
+
+export default Blogs;
